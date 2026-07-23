9b376b7e5969e9d8a82e33423cbe03ef71a9f1a5
salisai <sayedalishahshah5@gmail.com>
2026-07-23 15:47:04 +0500

    optimized the file uploading feature, streaming mode

--- bpe_tokenizer/io.py
+++ bpe_tokenizer/io.py
@@ -1,7 +1,7 @@
 from pathlib import Path 
 from typing import Iterator, Iterable 
 
-def stream_files(paths: Iterable[Path]) -> Iterator[sr]:
+def stream_files(paths: Iterable[Path]) -> Iterator[str]:
     """stream text from the list of files paths"""
     for path in paths: 
         with open(path, "r", encoding="utf-8") as f: 
